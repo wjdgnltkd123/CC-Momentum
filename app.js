6413d4a4ee961ceaa3ff557576e3adc2d29ee30a
DESKTOP-ES57QBV\deus <deus>
2025-01-06 15:54:25 +0900

    로그인 기능 추가: 사용자 이름 입력 및 인사 표시 구현

---
+++ app.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN = "hidden";
+const USERNAME_KEY = "username";
+
+const showGreeting = (username) => {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN);
+};
+
+const loginEvent = (event) => {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN);
+
+    showGreeting(username);
+
+    localStorage.setItem(USERNAME_KEY, username);
+};
+
+const savedusername = localStorage.getItem(USERNAME_KEY);
+
+if (savedusername === null || savedusername === "") {
+    loginForm.classList.remove(HIDDEN);
+    loginForm.addEventListener("submit", loginEvent);
+} else {
+    showGreeting(savedusername);
+}
