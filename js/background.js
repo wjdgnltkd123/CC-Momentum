7ae77e8c8bb70cca9775b01c7a11077aea830593
DESKTOP-ES57QBV\deus <deus>
2025-01-11 16:23:35 +0900

    스타일 개선 및 로그인 폼 구조 변경: CSS 스타일 추가 및 HTML 구조 재편성

--- js/background.js
+++ js/background.js
@@ -2,7 +2,8 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
 const choosenImage = images[randomNextInt(0, images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/background/${choosenImage}`;
-
-document.body.appendChild(bgImage);
+const bodyStyle = document.body.style;
+bodyStyle.backgroundImage = `url(img/background/${choosenImage})`;
+bodyStyle.backgroundSize = "cover";
+bodyStyle.backgroundPosition = "center";
+bodyStyle.backgroundRepeat = "no-repeat";
